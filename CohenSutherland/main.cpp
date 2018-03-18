f57184f949f9449ef8a27f5986e59ffa3a913385
Akashdeepsingh98 <akashdeepsingh98@gmail.com>
2018-03-18 20:47:23 +0530

    Cohen Sutherland Complete

--- CohenSutherland/main.cpp
+++ CohenSutherland/main.cpp
@@ -4,6 +4,11 @@
 #include <math.h>
 #include <dos.h>
 #define PI 3.1415
+#define TOP 1
+#define BOTTOM 2
+#define RIGHT 4
+#define LEFT 8
+#define INSIDE 0
 
 typedef struct Matrix
 {
@@ -18,6 +23,9 @@ void AcceptLines(Matrix *lines);
 void PrepareCodes(Matrix *lines, int xwmin,int ywmin, int xwmax,int ywmax);
 void AcceptWindow(int *xwmin,int *xwmax,int *ywmin,int *ywmax);
 void DrawWindow(int xwmin,int ywmin, int xwmax,int ywmax);
+void PointCode(int *code, int x,int y, int xwmin,int ywmin, int xwmax,int ywmax);
+void DrawLines(Matrix *lines);
+void PrintMatrix(Matrix *m);
 main()
 {
 	CS();
@@ -36,11 +44,12 @@ void CS()
 	int xwmin,xwmax,ywmin,ywmax;
 	AcceptLines(&lines);
 	AcceptWindow(&xwmin,&xwmax,&ywmin,&ywmax);
-	PrepareCodes(&lines,xwmin,xwmax,ywmin,ywmax);
+	PrepareCodes(&lines,xwmin,ywmin,xwmax,ywmax);
 	
 	//Graphical part only
 	InitGraph();
 	DrawWindow(xwmin,ywmin,xwmax,ywmax);
+	DrawLines(&lines);
 }
 
 void AcceptWindow(int *xwmin,int *xwmax,int *ywmin,int *ywmax)
@@ -79,27 +88,95 @@ void AcceptLines(Matrix *lines)
 	}
 }
 
+void PointCode(int *code, int x,int y, int xwmin,int ywmin, int xwmax,int ywmax)
+{
+	*code = 0;
+	*code += x<xwmin?LEFT:0;
+	*code += x>xwmax?RIGHT:0;
+	*code += y>ywmax?BOTTOM:0;
+	*code += y<ywmin?TOP:0;
+}
+
 void PrepareCodes(Matrix *lines,int xwmin,int ywmin, int xwmax,int ywmax)
 {
-	int nlines = lines->rows, row_iter,bit_iter;
-	int x1,y1,x2,y2,*code1,*code2;
+	int nlines = lines->rows, row_iter, *lineptr;
+	int x1,y1,x2,y2,counter,code;
+	float m, c;
 	for(row_iter=0;row_iter<nlines;row_iter++)
 	{
-		x1=lines->matrix[row_iter][0];
-		x2=lines->matrix[row_iter][2];
-		y1=lines->matrix[row_iter][1];
-		y2=lines->matrix[row_iter][3];
-		code1 = &lines->matrix[row_iter][4];
-		code2 = &lines->matrix[row_iter][5];
-		*code1 = x1<xwmin?1:0;
-		*code1 = x1>xwmax?1:0 + 10**code1;
-		*code1 = y1<ywmin?1:0 + 10**code1;
-		*code1 = y1>ywmax?1:0 + 10**code1;
-		*code2 = x2<xwmin?1:0;
-		*code2 = x2>xwmax?1:0 + 10**code2;
-		*code2 = y2<ywmin?1:0 + 10**code2;
-		*code2 = y2>ywmax?1:0 + 10**code2;
+		x1 = lines->matrix[row_iter][0];
+		y1 = lines->matrix[row_iter][1];
+		x2 = lines->matrix[row_iter][2];
+		y2 = lines->matrix[row_iter][3];
+		lineptr = lines->matrix[row_iter];
+		
+		PointCode(&lineptr[4],x1,y1,xwmin,ywmin,xwmax,ywmax);
+		PointCode(&lineptr[5],x2,y2,xwmin,ywmin,xwmax,ywmax);
+		PrintMatrix(lines);
+		if((lineptr[4] | lineptr[5]) == 0)
+		{
+			lineptr[6] = (int)'I';
+		}
+		else if((lineptr[4] & lineptr[5]) != 0)
+		{
+			lineptr[6] = (int)'O';
+		}
+		else
+		{
+			lineptr[6] = (int)'C';
+			m = ((float)(y2-y1))/(x2-x1);
+			c = y2 - m * x2;
+			if((lineptr[4] & LEFT) != 0)
+			{
+				lineptr[0] = xwmin+1;
+				lineptr[1] = m*(xwmin+1)+c;
+				PointCode(&lineptr[4],lineptr[0],lineptr[1],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[4] & TOP) != 0)
+			{
+				lineptr[1] = ywmin+1;
+				lineptr[0] = (ywmin+1 - c)/m;
+				PointCode(&lineptr[4],lineptr[0],lineptr[1],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[4] & BOTTOM) != 0)
+			{
+				lineptr[1] = ywmax-1;
+				lineptr[0] = (ywmax -1 - c)/m;
+				PointCode(&lineptr[4],lineptr[0],lineptr[1],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[4] & RIGHT) != 0)
+			{
+				lineptr[0] = xwmax-1;
+				lineptr[1] = m*(xwmax-1)+c;
+				PointCode(&lineptr[4],lineptr[0],lineptr[1],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[5] & LEFT) != 0)
+			{
+				lineptr[2] = xwmin+1;
+				lineptr[3] = m*(xwmin+1)+c;
+				PointCode(&lineptr[5],lineptr[2],lineptr[3],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[5] & TOP)!=0)
+			{
+				lineptr[3] = ywmin+1;
+				lineptr[2] = (ywmin +1- c)/m;
+				PointCode(&lineptr[5],lineptr[2],lineptr[3],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[5] & BOTTOM) != 0)
+			{
+				lineptr[3] = ywmax-1;
+				lineptr[2] = (ywmax -1- c)/m;
+				PointCode(&lineptr[5],lineptr[2],lineptr[3],xwmin,ywmin,xwmax,ywmax);
+			}
+			if((lineptr[5] & RIGHT) != 0)
+			{
+				lineptr[2] = xwmax-1;
+				lineptr[3] = m*(xwmax-1)+c;
+				PointCode(&lineptr[5],lineptr[2],lineptr[3],xwmin,ywmin,xwmax,ywmax);
+			}
+		}
 	}
+	PrintMatrix(lines);
 }
 
 void InitMatrix(Matrix *m,int r,int c, int ini)
@@ -115,4 +192,25 @@ void InitMatrix(Matrix *m,int r,int c, int ini)
 			m->matrix[row_iter][col_iter] = ini;
 	}
 	
+}
+
+void DrawLines(Matrix *lines)
+{
+	int linen;
+	for(linen=0;linen<lines->rows;linen++)
+	{
+		if(lines->matrix[linen][6] == (int)'I' || lines->matrix[linen][6] == (int)'C')
+			line(lines->matrix[linen][0],lines->matrix[linen][1],lines->matrix[linen][2],lines->matrix[linen][3]);
+	}
+}
+void PrintMatrix(Matrix *m)
+{
+	int r,c;
+	printf("\n");
+	for(r=0;r<m->rows;r++)
+	{
+		printf("\n");
+		for(c=0;c<m->columns;c++)
+			printf("%10d",m->matrix[r][c]);
+	}
 }
